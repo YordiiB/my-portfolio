40b722a8f20df072403a6879007cb84d5a03f46f
YordiiB <105393316+YordiiB@users.noreply.github.com>
2026-01-16 11:50:16 +0300

    removed image declaration

--- src/components/sections/Feedbacks.tsx
+++ src/components/sections/Feedbacks.tsx
@@ -13,7 +13,6 @@ const FeedbackCard: React.FC<{ index: number } & TTestimonial> = ({
   name,
   designation,
   company,
-  _image,
 }) => (
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
